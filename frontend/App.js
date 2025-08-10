3a90c4a5bf7fa378be96a8bd0308a3729a04ba69
Charanlokesh22 <charanlokesh2918@gmail.com>
2025-08-10 13:01:02 +0530

    Create App.js

---
+++ frontend/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ResearchForm from "./components/ResearchForm";
+
+function App() {
+  return (
+    <div>
+      <ResearchForm />
+    </div>
+  );
+}
+
+export default App;
